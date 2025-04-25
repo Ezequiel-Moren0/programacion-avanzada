916226e0bf074390d6c2edc2ff2c3d386a056109
Ezequiel-Moren0 <147450975+Ezequiel-Moren0@users.noreply.github.com>
2025-04-25 13:33:56 -0300

    Update Servidor.java

--- Tp Socket/Servidor-Cliente-Hilos/src/servidor/cliente/hilos/Servidor.java
+++ Tp Socket/Servidor-Cliente-Hilos/src/servidor/cliente/hilos/Servidor.java
@@ -1,48 +1,30 @@
 package servidor.cliente.hilos;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.*;
+import java.net.*;
 
 public class Servidor {
 
     public static void main(String[] args) {
-
         try {
             ServerSocket servidor = new ServerSocket(5000);
-            Socket sc;
-
-            System.out.println("El servidor se a iniciado");
-            System.out.println("-------------------------");
+            System.out.println("Servidor iniciado...");
 
             while (true) {
-
-                sc = servidor.accept(); // El servidor se queda parado hasta que encuentre un cliente
-
+                Socket sc = servidor.accept();
                 DataInputStream in = new DataInputStream(sc.getInputStream());
                 DataOutputStream out = new DataOutputStream(sc.getOutputStream());
 
-                out.writeUTF("Ingresa el nombre: ");
-                String nombreCliente = in.readUTF();
+                out.writeUTF("Escribe tu nombre:");
+                String nombre = in.readUTF();
 
-                out.writeUTF("Ingrese el apellido: ");
-                String apellidoCliente = in.readUTF();
+                System.out.println("Nuevo cliente: " + nombre);
 
-                ServidorHilo hilo = new ServidorHilo(in, out, nombreCliente, apellidoCliente);
+                ServidorHilo hilo = new ServidorHilo(in, out, nombre);
                 hilo.start();
-
-                System.out.println("Nueva conexión de cliente: " + nombreCliente + " " + apellidoCliente);
-
             }
 
-        } catch (IOException ex) {
-            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException e) {
         }
-
     }
-
 }
