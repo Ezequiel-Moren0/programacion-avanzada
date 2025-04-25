2166cbc5366ffb4a24b2186c63aa5784bde9d5ac
Ezequiel-Moren0 <147450975+Ezequiel-Moren0@users.noreply.github.com>
2025-04-25 13:34:36 -0300

    Update Cliente.java

--- Tp Socket/Servidor-Cliente-Hilos/src/servidor/cliente/hilos/Cliente.java
+++ Tp Socket/Servidor-Cliente-Hilos/src/servidor/cliente/hilos/Cliente.java
@@ -1,46 +1,40 @@
 package servidor.cliente.hilos;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Cliente {
 
     public static void main(String[] args) {
-
         try {
-            Scanner scanner = new Scanner(System.in);
-            scanner.useDelimiter("\n");
-
             Socket sc = new Socket("127.0.0.1", 5000);
-
             DataInputStream in = new DataInputStream(sc.getInputStream());
             DataOutputStream out = new DataOutputStream(sc.getOutputStream());
+            Scanner scanner = new Scanner(System.in);
 
-            String mensaje = in.readUTF();                    
-            System.out.println(mensaje);                    
-            String nombre = scanner.next();
-            out.writeUTF(nombre);                             
+            System.out.println(in.readUTF()); // pedir nombre
+            String nombre = scanner.nextLine();
+            out.writeUTF(nombre);
 
-            mensaje = in.readUTF();                           
-            System.out.println(mensaje);
-            String apellido = scanner.next();
-            out.writeUTF(apellido);
+            Thread recibir = new Thread(() -> {
+                try {
+                    while (true) {
+                        System.out.println(">> " + in.readUTF());
+                    }
+                } catch (IOException e) {
+                    System.out.println("Desconectado.");
+                }
+            });
+            recibir.start();
 
-            ClienteHilo hilo = new ClienteHilo(in, out);
+            while (true) {
+                String mensaje = scanner.nextLine();
+                out.writeUTF(mensaje);
+            }
 
-            hilo.start();
-            hilo.join();
-
-        } catch (IOException ex) {
-            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
 }
